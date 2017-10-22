6784f3ead9b2b72eeaecd843a4ea41c4bf68d376
Rob Bronson <robert.j.bronson@gmail.com>
2017-10-21 22:45:24 -0700

    added tox and fixed setup file to have proper modules.  tox now runs all tests to passing

--- setup.py
+++ setup.py
@@ -9,10 +9,13 @@ setup(
     package_dir={'': 'src'},
     author='Robert Bronson',
     author_email='robert.j.bronson@gmail.com',
-    py_modules=['rot_13', 'coordinates...... ** all my modules (.py files)**'],
+    py_modules=['_1s_0s_and_wildcards',
+                'coordinates_validator',
+                'grouped_by_commas',
+                'simple_pig_latin'],
     install_requires=[],
     extras_require={
-        'testing': ['pytest', 'pytest-cov', 'pytest-cov', 'tox'],
+        'testing': ['pytest', 'pytest-cov', 'tox'],
         'development': ['ipython']
     },
     entry_points={}
