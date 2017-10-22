b5c63e59ba9eb922fe778e7a1d94b77db6475218
Rob Bronson <robert.j.bronson@gmail.com>
2017-10-21 22:10:38 -0700

    removed more commented out code

--- src/test_1s_0s_and_wildcards.py
+++ src/test_1s_0s_and_wildcards.py
@@ -12,20 +12,6 @@ WILDCARD_LIST = [('101?', ['1010', '1011']),
                  ('11??0', ['11000', '11010', '11100', '11110'])]
 
 
-# test.describe("basic tests")
-
-# t1 = possibilities('101?')
-# t1.sort()
-# test.assert_equals(t1, ['1010', '1011'])
-
-# t2 = possibilities('10??')
-# t2.sort()
-# test.assert_equals(t2, ['1000', '1001', '1010', '1011'])
-
-# t3 = possibilities('1?1?')
-# t3.sort()
-# test.assert_equals(t3, ['1010', '1011', '1110', '1111'])
-
 @pytest.mark.parametrize("input_string, result", WILDCARD_LIST)
 def test_1s_and_0s_wildcards(input_string, result):
     """Pass values to function and see if they are valid."""
